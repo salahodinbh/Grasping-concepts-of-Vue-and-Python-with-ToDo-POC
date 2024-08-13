cc3a5600ba4e9016dc969d89bdf2ece6a68c6a0c
Stryfura <shilunett@gmail.com>
2024-08-14 06:02:52 +0900

    Quick changes

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  data() {
+    return {
+      message: "",
+    };
+  },
+  mounted() {
+    fetch("http://localhost:5000/")
+      .then((response) => response.text())
+      .then((data) => {
+        this.message = data;
+      });
+  },
+};
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+</script>
