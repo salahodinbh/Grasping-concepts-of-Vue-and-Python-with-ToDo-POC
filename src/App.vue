c6df042d826d0b27647288f0c23107016fbb6878
Stryfura <shilunett@gmail.com>
2024-08-18 16:11:08 +0900

    Added implementation of CRUD POC as a ToDo List

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
-    <h1>{{ message }}</h1>
-    <h1>{{ message_two }}</h1>
+    <!-- <h1>{{ message }}</h1> -->
+    <h1>To-Do List POC</h1>
+    <div v-for="(task, index) in tasks" :key="index">
+      <p>{{ task }}</p>
+      <button @click="removeTask(index)">Remove</button>
+    </div>
+    <input v-model="newTask" @keydown.enter="addTask" placeholder="Add a new task"/>
+    <button @click="addTask">Add</button>
   </div>
 </template>
 
@@ -9,23 +15,51 @@
 export default {
   data() {
     return {
-      message: "",
-      message_two: "yikes"
+      //message: ""
+      tasks: [],
+      newTask: ""
     };
   },
   mounted() {
-    fetch("http://localhost:5000/")
+    /**fetch("http://localhost:5000/")
       .then((response) => response.text())
       .then((data) => {
         this.message = data;
       });
-    fetch("/api")
-    .then((response) => response.text())
-    .then((data) => {
-        this.message_two = data;
-      });
+      */
+     this.fetchTasks();
   },
+  methods: {
+    addTask() {
+      if (this.newTask.trim() !== "") {
+        fetch("http://localhost:5000/tasks", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({ task: this.newTask })
+        })
+        .then(() => {
+          this.fetchTasks();
+          this.newTask = "";
+        });
+      }
+    },
+    removeTask(index) {
+      fetch(`http://localhost:5000/tasks/${index}`, {
+        method: "DELETE"
+      })
+      .then(() => {
+          this.fetchTasks();
+        })
+    },
+    fetchTasks() {
+      fetch("http://localhost:5000/tasks")
+        .then((response) => response.json())
+        .then((data) => {
+          this.tasks = data;
+        });
+    }
+  }
 };
-
-
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   devServer: {
     proxy: {
+      // For some reason, the proxy is not working properly.
+      // I'll just proceed using the plain localhost url for now.
       '/api': {
-        target: 'http://127.0.0.1:5000',
-        pathRewrite: { "/api": "^/api" },
-        changeOrigin: true,
-        logLevel: 'debug'
+        target: 'http://localhost:5000/',
+        changeOrigin: true
       },
     },
   },
